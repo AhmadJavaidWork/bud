282d1475f969ddcc163c7d44aed49bc666a0a3de
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-11-13 14:00:48 +0500

    added support for adding multiple comma seperated header values

--- header.go
+++ header.go
@@ -1,20 +1,23 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"strings"
+)
 
-type Header map[string]string
+type Header map[string][]string
 
 func (h Header) Add(key, value string) {
-	h[key] = value
+	h[key] = append(h[key], value)
 }
 
 func (h Header) Set(key, value string) {
-	h[key] = value
+	h[key] = []string{value}
 }
 
 func (h Header) Get(key string) string {
 	if v, ok := h[key]; ok {
-		return v
+		return strings.Join(v, ", ")
 	}
 	return ""
 }
@@ -23,7 +26,7 @@ func (h Header) String() string {
 	var out bytes.Buffer
 
 	for k, v := range h {
-		out.WriteString(k + ": " + v + "\r\n")
+		out.WriteString(k + ": " + strings.Join(v, ", ") + "\r\n")
 	}
 	out.WriteString("\r\n")
 
