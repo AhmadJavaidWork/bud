addd657917acf10d9a99dcf145f0e4e0462ac37e
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-11-13 13:59:57 +0500

    refactor

--- error_responses.go
+++ error_responses.go
@@ -1,21 +1,21 @@
 package main
 
-func closeConnectionHandler(w ResponseWriter, r *Request) {
+func closeConnectionHandler(w ResponseWriter) {
 	w.Header().Add("Connection", "close")
 }
 
 func notFoundHandler(w ResponseWriter, r *Request) {
 	w.WriteHeader(StatusNotFound)
-	closeConnectionHandler(w, r)
+	closeConnectionHandler(w)
 }
 
 func methodNotAllowed(w ResponseWriter, r *Request) {
 	w.WriteHeader(StatusMethodNotAllowed)
 	w.Header().Add("Allow", "GET")
-	closeConnectionHandler(w, r)
+	closeConnectionHandler(w)
 }
 
 func internalServerErrorHandler(w ResponseWriter, r *Request) {
 	w.WriteHeader(StatusInternalServerError)
-	closeConnectionHandler(w, r)
+	closeConnectionHandler(w)
 }
